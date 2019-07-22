12c57469b7854d522d90ecee346901bee0bf8d1d
artem-galas <artem.galas@gmail.com>
2019-07-22 23:07:58 +0300

    core: update application prefix
    Update app prefix.
    Add rules to tslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'crs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'veriff-test';
 }

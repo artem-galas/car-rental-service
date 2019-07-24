b8d89f9d3c710670f0fd9238a249a9f7ba775ed6
artem-galas <artem.galas@gmail.com>
2019-07-24 09:22:41 +0300

    feat(auth): add veriff-sdk

--- src/app/+auth/sign-up/sign-up.component.ts
+++ src/app/+auth/sign-up/sign-up.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 
+import Veriff from '@veriff/js-sdk';
+import { switchMap, takeUntil } from 'rxjs/operators';
+
+import { BaseComponent } from '~/framework';
 import { AuthService } from '../shared/services/auth.service';
 
 @Component({
@@ -9,25 +13,70 @@ import { AuthService } from '../shared/services/auth.service';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent extends BaseComponent implements OnInit {
   signUpForm: FormGroup;
+  veriff;
+
+  get fullName() {
+    return this.signUpForm.get('fullName').value;
+  }
+
+  get firstName() {
+    return this.fullName.split(' ')[0];
+  }
+
+  get lastName() {
+    return this.fullName.split(' ')[1];
+  }
+
+  get idNumber() {
+    return this.signUpForm.get('idNumber').value;
+  }
 
   constructor(private readonly fb: FormBuilder,
               private readonly authService: AuthService,
-              private readonly snackBar: MatSnackBar) { }
+              private readonly snackBar: MatSnackBar,
+              private readonly render: Renderer2) {
+    super();
+  }
 
   ngOnInit() {
     this.buildForm();
+    this.initializeVeriff();
   }
 
   submitSignUpForm() {
-    this.authService.signUp(this.signUpForm.value)
-      .subscribe(
-        user => console.log(user),
-        (message: string) => {
-          this.snackBar.open(message, 'Ok');
-        }
-      );
+    if (this.signUpForm.valid) {
+      this.authService.signUp(this.signUpForm.value)
+        .pipe(
+          switchMap(() => {
+            const successSnackBar = this.snackBar.open('Your identity should be verified.', 'Verify');
+
+            return successSnackBar.onAction();
+          }),
+          takeUntil(this.ngUnsubscribe)
+        )
+        .subscribe(
+          () => {
+            this.veriff.setParams({
+              person: {
+                givenName: this.firstName,
+                lastName: this.lastName,
+                idNumber: this.idNumber
+              }
+            });
+            this.veriff.mount();
+
+            const veriffButton = this.render.selectRootElement('#veriff-submit-btn') as HTMLInputElement;
+            veriffButton.click();
+        },
+          (message: string) => {
+            this.snackBar.open(message, 'Ok');
+          }
+        );
+    } else {
+      this.snackBar.open('Form is not valid', 'Ok');
+    }
   }
 
   private buildForm() {
@@ -38,9 +87,23 @@ export class SignUpComponent implements OnInit {
       fullName: [null, Validators.compose(
         [Validators.required]
       )],
+      idNumber: [null, Validators.compose(
+        [Validators.required]
+      )],
       password: [null, Validators.compose(
         [Validators.required, Validators.minLength(6)]
       )],
+    });
+  }
+
+  private initializeVeriff() {
+    this.veriff = Veriff({
+      env: 'production',
+      apiKey: 'c273bb49-3be7-447e-a7cc-3d00cb9cfdeb',
+      parentId: 'veriff-root',
+      onSession: (err, response) => {
+        location.replace(response.verification.url);
+      }
     });
   }
 

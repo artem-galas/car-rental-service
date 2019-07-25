c3be9398f3aed34bc6c99d1d96f0197921c562df
Artem Galas <artem.galas@gmail.com>
2019-07-25 12:36:07 +0300

    core: add firebase hosting and travis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
   token: 'jwtToken',
-  apiUrl: ''
+  apiUrl: '',
+  firebase: {
+    apiKey: 'AIzaSyA7TbPZc1pLxakc090RsfFDrxGuXlGV6-M',
+    authDomain: 'fire-auth-8fd44.firebaseapp.com',
+    databaseURL: 'https://fire-auth-8fd44.firebaseio.com',
+    projectId: 'fire-auth-8fd44',
+    storageBucket: 'fire-auth-8fd44.appspot.com',
+    messagingSenderId: '893629072234',
+    appId: '1:893629072234:web:9ed3f9c2e2690fdc'
+  }
 };
